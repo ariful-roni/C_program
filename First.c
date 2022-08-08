198eb3cbf0ed33f0659085521bae2e5844874015
ariful_roni <ariful17@cse.pstu.ac.bd>
2022-08-08 07:12:00 +0600

    My first C program on git!

---
+++ First.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int check;
+  printf("Press 1 to see my identity:");
+  scanf("%d",&check);
+  if(check==1)
+   {
+     printf("My name is Md. Ariful Islam Roni!\n");
+     }
+   else
+      printf("Nothing to show!\n");
+      
+      return 0;
+      }
